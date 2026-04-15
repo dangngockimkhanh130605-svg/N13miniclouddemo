7b455780d72320bbde9c67432d6d10852375af57
S52300206 <52300206@student.tdtu.edu.vn>
2026-04-15 15:39:18 +0700

    Hoan thanh phan mo rong 4 voi Keycloak va secure API

--- application-backend-server/app.py
+++ application-backend-server/app.py
@@ -3,9 +3,12 @@ import time, requests, os, json
 from jose import jwt
 import mysql.connector
 
-ISSUER   = os.getenv("OIDC_ISSUER",   "http://authentication-identity-server:8080/realms/master")
-AUDIENCE = os.getenv("OIDC_AUDIENCE", "myapp")
-JWKS_URL = f"{ISSUER}/protocol/openid-connect/certs"
+ISSUER   = os.getenv("OIDC_ISSUER", "http://localhost:8081/realms/realm_sv001")
+AUDIENCE = os.getenv("OIDC_AUDIENCE", "account")
+JWKS_URL = os.getenv(
+    "OIDC_JWKS_URL",
+    "http://authentication-identity-server:8080/realms/realm_sv001/protocol/openid-connect/certs"
+)
 
 _JWKS = None; _TS = 0
 def get_jwks():
@@ -23,13 +26,45 @@ def hello(): return jsonify(message="Hello from App Server!")
 
 @app.get("/secure")
 def secure():
-    auth = request.headers.get("Authorization","")
+    auth = request.headers.get("Authorization", "")
     if not auth.startswith("Bearer "):
         return jsonify(error="Missing Bearer token"), 401
-    token = auth.split(" ",1)[1]
+
+    token = auth.split(" ", 1)[1]
+
     try:
-        payload = jwt.decode(token, get_jwks(), algorithms=["RS256"], audience=AUDIENCE, issuer=ISSUER)
-        return jsonify(message="Secure resource OK", preferred_username=payload.get("preferred_username"))
+        unverified_header = jwt.get_unverified_header(token)
+        kid = unverified_header.get("kid")
+
+        jwks = get_jwks()
+        rsa_key = None
+
+        for key in jwks.get("keys", []):
+            if key.get("kid") == kid:
+                rsa_key = {
+                    "kty": key.get("kty"),
+                    "kid": key.get("kid"),
+                    "use": key.get("use"),
+                    "n": key.get("n"),
+                    "e": key.get("e"),
+                }
+                break
+
+        if rsa_key is None:
+            return jsonify(error="Unable to find appropriate key"), 401
+
+        payload = jwt.decode(
+            token,
+            rsa_key,
+            algorithms=["RS256"],
+            audience=AUDIENCE,
+            issuer=ISSUER
+        )
+
+        return jsonify(
+            message="Secure resource OK",
+            preferred_username=payload.get("preferred_username")
+        )
     except Exception as e:
         return jsonify(error=str(e)), 401
     
